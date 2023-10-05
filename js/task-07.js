8a0f75209a6f960beff52181cfdc49ada1bbcbae
Andriy Razumnykh <andriyrazumnykh@gmail.com>
2023-10-05 04:25:03 +0300

    :)

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const spanText = document.querySelector('#text');
 
 const inputSizeControl = document.querySelector('#font-size-control');
+spanText.style.fontSize = `${inputSizeControl.value}px`
 
 inputSizeControl.addEventListener('input', event => {
   const fontSize = inputSizeControl.value;
